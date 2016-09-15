ddfaca7f62bc2b4c0386f839e8b1170ee87a9e6f
Gustav <sir.gustav.the.coder@gmail.com>
2016-09-15 22:42:10 +0200

    initial commit

---
+++ android/src/com/madeso/spaceman/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.madeso.spaceman;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.madeso.spaceman.SpacemanGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpacemanGame(), config);
+	}
+}
